d9cdfca4be679c1af113ad108ab0e785d00cc259
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-06-01 05:25:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,9 @@
 def prime?(num) # Add  code here!
-    
+    unless n.is_a? Integer
+  puts "That's not an integer."
+  if n%2 !=0 && n!=2
+    puts "#{n} is prime!"
+  else
+    puts "#{n} is not prime."
+  end
 end
